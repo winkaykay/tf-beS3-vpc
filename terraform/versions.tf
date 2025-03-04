e85bbca2fbd0073acbc73a63f20e683745f877db
Win Kay Kay Khine <winkaykay.khine@navixsolutions.com>
2025-03-04 13:22:07 +0800

    changes in versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -12,5 +12,5 @@ provider "aws" {
   #   profile = "master-programmatic-admin"
   #   access_key = data.vault_kv_secret_v2.hellocloud-aws-creds.data["aws_access_key_id"]
   #   secret_key = data.vault_kv_secret_v2.hellocloud-aws-creds.data["aws_secret_access_key"]
-  region = "ap-southeast-1"
+  #   region = "ap-southeast-1"
 }
